78a154bb9363cf6f2f28ba25680552c76cc3df92
TA2525 <70610897+TA2525@users.noreply.github.com>
2022-04-03 08:39:01 -0500

    Time: 1 ms, Memory: 50.3 MB - LeetHub

--- reverse-string/reverse-string.java
+++ reverse-string/reverse-string.java
@@ -4,14 +4,14 @@ class Solution {
     }
     
     private void helper(int index, char[] s) {
-        if (index >= s.length) {
+        if (index >= s.length/2) {
             return;
         }
+
+        char t = s[s.length - index - 1];
+        s[s.length - index - 1] = s[index];
+        s[index] = t;
+        
         helper(index + 1, s);
-        if (index >= s.length/2) {
-            char t = s[s.length - index - 1];
-            s[s.length - index - 1] = s[index];
-            s[index] = t;
-        }
     }
 }
