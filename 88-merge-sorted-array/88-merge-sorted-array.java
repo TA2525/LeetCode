5786e4c9290ea9a434906c94e4106bae87d144a6
TA2525 <70610897+TA2525@users.noreply.github.com>
2022-08-23 21:14:12 -0400

    Time: 1 ms (41.20%), Space: 42.5 MB (77.71%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int one = 0;
+        int two = 0;
+        
+        int[] arr = new int[m+n];
+        int i = 0;
+        
+        while (one < m && two < n) {
+            if (nums1[one] <= nums2[two]) {
+                arr[i] = nums1[one];
+                one++;
+            } else {
+                arr[i] = nums2[two];
+                two++;
+            }
+            i++;
+        }
+        
+        while (one < m) {
+            arr[i] = nums1[one];
+            one++;
+            i++;
+        }
+        
+        while (two < n) {
+            arr[i] = nums2[two];
+            two++;
+            i++;
+        }
+
+        
+        
+        for (int t = 0; t < m+n; t++) {
+            nums1[t] = arr[t];
+        }
+    }
+}
