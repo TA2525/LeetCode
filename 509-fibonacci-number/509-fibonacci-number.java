9c2041cc0d238fd60ccb29d8a5c9ece8718a264d
TA2525 <70610897+TA2525@users.noreply.github.com>
2022-09-06 12:35:05 -0400

    Time: 0 ms (100.00%), Space: 42 MB (5.06%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,14 +1,16 @@
 class Solution {
     public int fib(int n) {
-        int c = 1;
-        int first = 0;
-        int second = 1;
         if (n == 0) {
             return 0;
         }
         if (n == 1) {
             return 1;
         }
+        
+        
+        int c = 1;
+        int first = 0;
+        int second = 1;
         while (c < n) {
             int sum = first + second;
             first = second;
